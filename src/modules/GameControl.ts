d7e3673eab1839d33eb67b9df9f5cec9dbe9583e
Yuu <2087791511@qq.com>
2023-03-03 21:58:25 +0800

    All completed

--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -15,6 +15,9 @@ class GameControl{
   // 创建一个属性来存储蛇的移动方向(也就是按键方向)
   direction: string='';
 
+  // 控制游戏是否结束
+  isLive =true;
+
   constructor() {
     this.snake = new Snake();
     this.food = new Food();
@@ -25,11 +28,69 @@ class GameControl{
   // 游戏的初始化方法，调用后游戏即开始
   init(){
     document.addEventListener('keydown',this.keydownHandler.bind(this));
+    this.run();
   }
 
   // 创建一个键盘按下的响应函数
   keydownHandler(event:KeyboardEvent){
-    this.direction = event.key
+    this.direction = event.key;
+  }
+
+  // 创建一个控制蛇移动的方法
+  run(){
+    /* 根据方向（this.direction）来使蛇的位置改变
+      向上 top 减少
+      向下 top 增加
+      向左 left 减少
+      向右 right 增加
+    */
+
+    // 获取蛇现在的坐标
+    let X = this.snake.X;
+    let Y = this.snake.Y;
+
+    switch(this.direction){
+      case "ArrowUp":
+        Y -= 10;
+        break;
+      
+      case "ArrowDown":
+        Y += 10;
+        break;
+
+      case "ArrowLeft":
+        X -= 10;
+        break;
+      
+      case "ArrowRight": 
+        X += 10;
+        break;
+    }
+
+    // 检查蛇是否迟到了食物
+    this.checkEat(X,Y);
+
+    try{
+      this.snake.X = X;
+      this.snake.Y = Y;
+    }catch(e:any){
+      alert(e.message);
+      this.isLive = false;
+    }
+    
+
+    this.isLive && setTimeout(this.run.bind(this),300-(this.scorePanel.level-1)*30)
+  }
+
+  // 定义一个方法用来检查蛇是否迟到食物
+  checkEat(X:number, Y:number){
+    if(X === this.food.X && Y === this.food.Y){
+      console.log('吃到食物了');
+      this.food.change();
+
+      this.scorePanel.addScore();
+      this.snake.addBody();
+    }
   }
 
 
--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -3,7 +3,7 @@ class Snake{
 
   head: HTMLElement;
   // 蛇的身体（包括蛇头）
-  bodies:HTMLCollection
+  bodies:HTMLCollection;
   element: HTMLElement;
 
   constructor() {
@@ -25,18 +25,92 @@ class Snake{
 
   // 设置蛇头的坐标
   set X(value:number){
+    if(this.X === value) return;
+    
+    if(value<0||value>290){
+      // 进入判断说明蛇撞墙了
+      throw new Error('蛇撞墙了！');
+    }
+
+    // 修改x时，蛇在左右移动，蛇在向左移动时，不能向右调头，反之亦然
+    if(this.bodies[1]&&(this.bodies[1] as HTMLElement).offsetLeft === value){
+      if(value>this.X){
+        // 说明此时蛇正在向左走
+        value = this.X -10;
+      }else{
+        value = this.X + 10;
+      }
+    }
+
+    // 移动身体
+    this.moveBody();
+
     this.head.style.left = value+'px';
+    // 检查有没有撞到自己
+    this.checkHeadBody();
+
   }
 
   // 设置蛇头的Y轴坐标
   set Y(value:number){
+    if(this.Y === value) return ;
+
+    if(value<0||value>290){
+      // 进入判断说明蛇撞墙了
+      throw new Error('蛇撞墙了！');
+    }
+
+    // 修改y时，蛇在上下移动，蛇在向下移动时，不能向上调头，反之亦然
+    if(this.bodies[1]&&(this.bodies[1] as HTMLElement).offsetTop === value){
+      if(value>this.Y){
+        value = this.Y - 10;
+      }else{
+        value = this.Y + 10;
+      }
+    }
+
+
+
+    // 移动身体
+    this.moveBody();
+
     this.head.style.top = value+'px';
+    // 检查有没有撞到自己
+    this.checkHeadBody();
+  }
+
+
+  // 检查蛇头是否撞到了身体
+  checkHeadBody(){
+    for(let i=1;i<this.bodies.length;i++){
+      let bd = this.bodies[i] as HTMLElement;
+      if(this.X === bd.offsetLeft && this.Y === bd.offsetTop){
+        throw new Error("撞到自己了");
+      }
+    }
   }
 
   // 蛇增加身体的方法
   addBody(){
     // 加在最后
     this.element.insertAdjacentHTML("beforeend", "<div></div>")
+  }
+
+
+  // 添加一个蛇身体移动的方法
+  moveBody() {
+    
+    /*
+    将后面的身体设置为前边身体的位置
+    */
+   // 遍历获取所有的身体
+   for(let i=this.bodies.length-1;i>0;i--){
+    let X = (this.bodies[i-1] as HTMLElement).offsetLeft;
+    let Y = (this.bodies[i-1] as HTMLElement).offsetTop;
+
+    (this.bodies[i] as HTMLElement).style.left = X +'px';
+    (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+   }
   }
 }
 
